cad7b32521b1eee72a5187dbaa03b06938ce203a
Thomas Baustert <business@thomasbaustert.de>
2017-02-06 18:30:44 +0100

    Add test for create_deck

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -5,4 +5,8 @@ defmodule CardsTest do
   # test "the truth" do
   #   assert 1 + 1 == 2
   # end
+  test "create_deck make 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
+  end
 end
